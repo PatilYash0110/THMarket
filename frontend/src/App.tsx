1bae2b19f7beddf83cf06930767a6041c44eb893
Yash Patil <yash.umesh.patil@mnd.thm.de>
2026-09-01 12:15:22 +0100

    feat(listings): add listing detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { NotFound } from './pages/NotFound'
 import { Home } from './pages/Home'
+import { ListingDetail } from './pages/ListingDetail'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-      <Route index element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="listing/:id" element={<ListingDetail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
 }
+
