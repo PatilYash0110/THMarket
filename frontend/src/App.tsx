7146e47a8107b91b661fb775e73dcd20763ba848
Yash Patil <yash.umesh.patil@mnd.thm.de>
2026-09-01 12:21:35 +0100

    feat(auth): add registration page with THM email validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { NotFound } from './pages/NotFound'
 import { Home } from './pages/Home'
 import { ListingDetail } from './pages/ListingDetail'
 import { Login } from './pages/Login'
+import { Register } from './pages/Register'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="listing/:id" element={<ListingDetail />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
