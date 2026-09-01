edf2bb17186037da314e54035976d7342bb867d1
Yash Patil <yash.umesh.patil@mnd.thm.de>
2026-09-01 12:18:39 +0100

    feat(auth): add login page with demo accounts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Layout } from './components/Layout'
 import { NotFound } from './pages/NotFound'
 import { Home } from './pages/Home'
 import { ListingDetail } from './pages/ListingDetail'
+import { Login } from './pages/Login'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="listing/:id" element={<ListingDetail />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
